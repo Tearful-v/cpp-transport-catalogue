45d957bfd829994f702a4a91690896bd974c15e8
tearful <popov.timofey.08@gmail.com>
2026-09-23 16:11:50 +0300

    декомпозировал

--- transport-catalogue/transport_router.cpp
+++ transport-catalogue/transport_router.cpp
@@ -1,77 +1,86 @@
 #include "transport_router.h"
 
 namespace transport_router {
-    using Weight = double;
-    using Graph = graph::DirectedWeightedGraph<Weight>;
-    using Router = graph::Router<Weight>;
-    using VertexId = size_t;
 
-    Graph TransportRouter::BuildGraph(const transport_catalogue::TransportCatalogue& catalogue) {
-        const auto& stops = catalogue.GetAllStops();
-        Graph graph(stops.size());
-        VertexId id = 0;
+namespace {
 
-        for (const auto& stop : stops) {
-            stop_to_vertex_[stop.name] = id;
-            ++id;
-        }
+constexpr double METERS_PER_KILOMETER = 1000.0;
+constexpr double MINUTES_PER_HOUR = 60.0;
 
-        const double meters_per_minute = settings_.bus_speed * 1000.0 / 60.0;
+}  // namespace
 
-        const auto& buses = catalogue.GetAllBuses();
-        for (const auto& bus : buses) {
-            const auto& route = bus.route;
+TransportRouter::Graph TransportRouter::BuildGraph(
+    const transport_catalogue::TransportCatalogue& catalogue) {
+    AssignVertexIds(catalogue);
 
-            for (size_t from = 0; from < route.size(); ++from) {
-                int total_distance = 0;
-
-                for (size_t to = from + 1; to < route.size(); ++to) {
-                    total_distance += catalogue.GetStopsDistance(route[to - 1]->name, route[to]->name);
-                    const double ride_time = total_distance / meters_per_minute;
-                    const double total_time = settings_.wait_time + ride_time;
-                    graph.AddEdge({stop_to_vertex_[route[from]->name],
-                                stop_to_vertex_[route[to]->name],
-                                total_time});
-
-                    info_.push_back({
-                        bus.name,
-                        route[from]->name,
-                        static_cast<int>(to - from),
-                        ride_time
-                    });
-                }
-            }
-        }
-
-        return graph;
-    }
-
-    std::optional<RouteResult> TransportRouter::BuildRoute(
-                            std::string_view from, std::string_view to) const {
-        auto it_from = stop_to_vertex_.find(from);
-        auto it_to = stop_to_vertex_.find(to);
-        if (it_from == stop_to_vertex_.end()
-            || it_to == stop_to_vertex_.end()) {
-                return std::nullopt;
-            }
-
-        const auto route = router_.BuildRoute(
-            it_from->second,
-            it_to->second
-        );
-
-        if(!route) {
-            return std::nullopt;
-        }
-
-        RouteResult result;
-        result.total_time = route->weight;
-        result.edges.reserve(route->edges.size());
-        for (graph::EdgeId edge_id : route->edges) {
-            result.edges.push_back(info_[edge_id]);
-        }
-
-        return result;
-    }
-
+    Graph graph(catalogue.GetAllStops().size());
+    AddBusEdges(graph, catalogue);
+    return graph;
 }
+
+void TransportRouter::AssignVertexIds(
+    const transport_catalogue::TransportCatalogue& catalogue) {
+    graph::VertexId id = 0;
+    for (const auto& stop : catalogue.GetAllStops()) {
+        stop_to_vertex_.emplace(stop.name, id++);
+    }
+}
+
+void TransportRouter::AddBusEdges(
+    Graph& graph, const transport_catalogue::TransportCatalogue& catalogue) {
+    const double meters_per_minute =
+        settings_.bus_speed * METERS_PER_KILOMETER / MINUTES_PER_HOUR;
+
+    for (const auto& bus : catalogue.GetAllBuses()) {
+        const auto& route = bus.route;
+        for (size_t from = 0; from < route.size(); ++from) {
+            int total_distance = 0;
+            for (size_t to = from + 1; to < route.size(); ++to) {
+                total_distance += catalogue.GetStopsDistance(route[to - 1]->name,
+                                                            route[to]->name);
+                const double ride_time = total_distance / meters_per_minute;
+                const double total_time = settings_.wait_time + ride_time;
+                const graph::EdgeId edge_id = graph.AddEdge({
+                    stop_to_vertex_.at(route[from]->name),
+                    stop_to_vertex_.at(route[to]->name),
+                    total_time,
+                });
+
+                info_.emplace(edge_id,
+                            EdgeInfo{bus.name,
+                                    route[from]->name,
+                                    static_cast<int>(to - from),
+                                    ride_time});
+            }
+        }
+    }
+}
+
+std::optional<transport_router::RouteResult> TransportRouter::BuildRoute(
+    std::string_view from, std::string_view to) const {
+    const auto it_from = stop_to_vertex_.find(from);
+    const auto it_to = stop_to_vertex_.find(to);
+    if (it_from == stop_to_vertex_.end() || it_to == stop_to_vertex_.end()) {
+        return std::nullopt;
+    }
+
+    const auto route = router_.BuildRoute(it_from->second, it_to->second);
+    if (!route) {
+        return std::nullopt;
+    }
+
+    transport_router::RouteResult result;
+    result.total_time = route->weight;
+    result.items.reserve(route->edges.size() * 2);
+    for (graph::EdgeId edge_id : route->edges) {
+        const EdgeInfo& edge = info_.at(edge_id);
+        result.items.emplace_back(
+            transport_router::WaitItem{edge.wait_stop, settings_.wait_time});
+        result.items.emplace_back(
+            transport_router::BusItem{edge.bus_name, edge.span_count, edge.ride_time});
+    }
+
+    return result;
+}
+
+}  //transport_router
--- transport-catalogue/transport_router.h
+++ transport-catalogue/transport_router.h
@@ -4,51 +4,72 @@
 #include "graph.h"
 #include "transport_catalogue.h"
 
-#include <unordered_map>
+#include <optional>
 #include <string>
 #include <string_view>
+#include <unordered_map>
+#include <utility>
+#include <variant>
 #include <vector>
-#include <optional>
 
 namespace transport_router {
 
-    struct RouterSettings {
-        double wait_time;
-        double bus_speed;
+struct RouterSettings {
+    double wait_time;
+    double bus_speed;
+};
+
+struct WaitItem {
+    std::string_view stop_name;
+    double time = 0.0;
+};
+
+struct BusItem {
+    std::string_view bus_name;
+    int span_count = 0;
+    double time = 0.0;
+};
+
+using RouteItem = std::variant<WaitItem, BusItem>;
+
+struct RouteResult {
+    double total_time = 0.0;
+    std::vector<RouteItem> items;
+};
+
+class TransportRouter {
+public:
+    using Weight = double;
+    using Graph = graph::DirectedWeightedGraph<Weight>;
+    using Router = graph::Router<Weight>;
+
+    explicit TransportRouter(RouterSettings settings,
+                             const transport_catalogue::TransportCatalogue& catalogue)
+        : settings_(std::move(settings))
+        , graph_(BuildGraph(catalogue))
+        , router_(graph_) {
+    }
+
+    std::optional<RouteResult> BuildRoute(std::string_view from, std::string_view to) const;
+
+private:
+    struct EdgeInfo {
+        std::string_view bus_name;
+        std::string_view wait_stop;
+        int span_count = 0;
+        double ride_time = 0.0;
     };
 
-    struct RouteResult {
-        double total_time = 0.0;
-        std::vector<graph::EdgeInfo> edges;
-    };
+    Graph BuildGraph(const transport_catalogue::TransportCatalogue& catalogue);
+    void AssignVertexIds(const transport_catalogue::TransportCatalogue& catalogue);
+    void AddBusEdges(Graph& graph, const transport_catalogue::TransportCatalogue& catalogue);
 
-    class TransportRouter {
-    public:
-        using Weight = double;
-        using Graph = graph::DirectedWeightedGraph<Weight>;
-        using Router = graph::Router<Weight>;
+    RouterSettings settings_;
+    std::unordered_map<std::string_view, graph::VertexId> stop_to_vertex_;
+    std::unordered_map<graph::EdgeId, EdgeInfo> info_;
 
-        explicit TransportRouter (RouterSettings settings,
-                const transport_catalogue::TransportCatalogue& catalogue) :
-                settings_(std::move(settings)), graph_(BuildGraph(catalogue)),
-                router_(graph_) {}
+    Graph graph_;
+    Router router_;
+};
 
-        std::optional<RouteResult> BuildRoute(std::string_view from,
-                                std::string_view to) const;
-
-        double GetWaitTime() const {
-            return settings_.wait_time;
-        }
-
-    private:
-        RouterSettings settings_;
-        Graph BuildGraph(const transport_catalogue::TransportCatalogue& catalogue);
-
-        std::unordered_map<std::string_view, graph::VertexId> stop_to_vertex_;
-        std::vector<graph::EdgeInfo> info_;
-
-        Graph graph_;
-        Router router_;
-    };
-
-} //transport_router
+}  // namespace transport_router
